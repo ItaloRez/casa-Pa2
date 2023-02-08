bf34d891bbea806f6d38c6a585290c30b73c0c3a
ItaloRez <italo.rezende@sharkdev.com.br>
2023-02-08 14:22:36 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,527 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.995638248012181,
+        "pitch": 0.16044678639494236,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.654855791728906,
+          "pitch": 0.249283394439356,
+          "rotation": 0,
+          "target": "8-sala"
+        },
+        {
+          "yaw": -1.5015858672110785,
+          "pitch": 0.2462932613054285,
+          "rotation": 0,
+          "target": "10-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-quarto-fundo",
+      "name": "Quarto fundo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.45426044684884914,
+        "pitch": 0.08230256306307027,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23374156648959854,
+          "pitch": 0.17453492730139963,
+          "rotation": 0,
+          "target": "2-banheiro-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-banheiro-entrada",
+      "name": "Banheiro Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.1713216863455447,
+        "pitch": 0.12161093595995709,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8421787527638855,
+          "pitch": 0.2496323377269043,
+          "rotation": 0,
+          "target": "3-banheiro-dentro"
+        },
+        {
+          "yaw": 2.3101240003362733,
+          "pitch": 0.2957152298026031,
+          "rotation": 0,
+          "target": "4-corredor-meio"
+        },
+        {
+          "yaw": 1.5173662282038212,
+          "pitch": 0.4232493137301887,
+          "rotation": 0,
+          "target": "1-quarto-fundo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-banheiro-dentro",
+      "name": "Banheiro dentro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.0437135376865996,
+        "pitch": 0.4010684811417988,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6213509444856484,
+          "pitch": 0.3670849938663192,
+          "rotation": 0,
+          "target": "2-banheiro-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-corredor-meio",
+      "name": "Corredor meio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5948396597005896,
+        "pitch": 0.3977500705927337,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08292091992703376,
+          "pitch": 0.26674292025943913,
+          "rotation": 0,
+          "target": "7-quarto-frente"
+        },
+        {
+          "yaw": 3.0738923958135347,
+          "pitch": 0.36909284638653617,
+          "rotation": 0,
+          "target": "5-quarto-suite"
+        },
+        {
+          "yaw": -1.4898432449733363,
+          "pitch": 0.26166236698131584,
+          "rotation": 0,
+          "target": "2-banheiro-entrada"
+        },
+        {
+          "yaw": 1.6751053193368763,
+          "pitch": 0.23096433803946503,
+          "rotation": 0,
+          "target": "8-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-quarto-suite",
+      "name": "Quarto suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.108940240260532,
+        "pitch": 0.32434389625253957,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6968324499405751,
+          "pitch": 0.29262206403978475,
+          "rotation": 0,
+          "target": "4-corredor-meio"
+        },
+        {
+          "yaw": -1.278048723316516,
+          "pitch": 0.208761561634482,
+          "rotation": 0,
+          "target": "6-suite"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-suite",
+      "name": "Suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.6519713082621381,
+        "pitch": 0.31599204244579404,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.234605980533578,
+          "pitch": 0.2475597877931719,
+          "rotation": 0,
+          "target": "5-quarto-suite"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-quarto-frente",
+      "name": "Quarto Frente",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.3194926683637416,
+        "pitch": 0.4846354603659542,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6962749070553311,
+          "pitch": 0.2868784402662232,
+          "rotation": 0,
+          "target": "4-corredor-meio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.8790080456072715,
+        "pitch": 0.29701585115043727,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5289876755776586,
+          "pitch": 0.13400744542164134,
+          "rotation": 0,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": 2.4576993796467663,
+          "pitch": 0.23044820987813708,
+          "rotation": 0,
+          "target": "4-corredor-meio"
+        },
+        {
+          "yaw": -2.16030582587163,
+          "pitch": 0.1759458199774464,
+          "rotation": 0,
+          "target": "9-varanda"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-varanda",
+      "name": "Varanda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.9504253440684414,
+        "pitch": 0.3145614290979193,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8641689387347515,
+          "pitch": 0.10047696480704182,
+          "rotation": 0,
+          "target": "8-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-cozinha",
+      "name": "Cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 4.900000405427818e-9,
+        "pitch": 0,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6041901140634778,
+          "pitch": 0.17448627909379155,
+          "rotation": 0,
+          "target": "11-area-de-servio"
+        },
+        {
+          "yaw": -2.366521381617753,
+          "pitch": 0.14010348502989523,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-area-de-servio",
+      "name": "Area de serviço",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8330587554167064,
+        "pitch": 0.38346460188922826,
+        "fov": 1.3496227372390412
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2062855244564226,
+          "pitch": 0.08521801086365599,
+          "rotation": 0,
+          "target": "10-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Casa-pa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
